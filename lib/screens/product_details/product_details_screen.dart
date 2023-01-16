61cdb434a48c8bbaf3bc3af71e2dbfd51f7167eb
mamhesam <mamhesam1@gmail.com>
2023-01-16 13:39:10 +0330

    use provider in my details product screen

--- lib/screens/product_details/product_details_screen.dart
+++ lib/screens/product_details/product_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../provider/products.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   //final String title;
@@ -7,8 +9,13 @@ class ProductDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)?.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
+      appBar: AppBar(title: Text(loadedProduct.title)),
     );
   }
 }
